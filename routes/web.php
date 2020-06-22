3549744a3e7ae1b755ceceaf57dcf3fcd7871ead
Ranga Lakshitha <ranga@thesanmark.com>
2020-06-22 11:28:03 +0530

    ..

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'HomeController@show');
+Route::post('/', 'HomeController@storePhoneNumber');
+Route::post('/custom', 'HomeController@sendCustomMessage');
